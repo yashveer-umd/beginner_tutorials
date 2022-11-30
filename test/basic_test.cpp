44a594b97d710e35fcaee00e3c6d40d93705bfd6
yashveer jain <yashveer@umd.edu>
2022-11-30 02:22:40 -0500

    test cases based on gtest

---
+++ test/basic_test.cpp
@@ -0,0 +1,70 @@
+// Description: Test if a simple task plan works
+#include <gtest/gtest.h>
+#include <stdlib.h>
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "beginner_tutorials/srv/change_string.hpp"
+// #include "rclcpp/logger.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+namespace minimal_integration_test {
+class TaskPlanningFixture : public testing::Test {
+ public:
+  TaskPlanningFixture() : node_(std::make_shared<rclcpp::Node>("basic_test")) {
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH CONSTRUCTOR!!");
+  }
+
+  void SetUp() override {
+    // Setup things that should occur before every test instance should go here
+    RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH SETUP!!");
+  }
+
+  void TearDown() override { std::cout << "DONE WITH TEARDOWN" << std::endl; }
+
+ protected:
+  rclcpp::Node::SharedPtr node_;
+};
+
+TEST(TaskPlanningFixture, TrueIsTrueTest) {
+  std::cout << "TEST BEGINNING!!" << std::endl;
+  EXPECT_TRUE(true);
+}
+
+TEST(test_services_client, test_string_change) {
+  auto node = rclcpp::Node::make_shared("test_services_client_string_change");
+
+  auto client = node->create_client<beginner_tutorials::srv::ChangeString>(
+      "service_node");
+  auto request =
+      std::make_shared<beginner_tutorials::srv::ChangeString::Request>();
+  request->ip = "Testing";
+
+  if (!client->wait_for_service(20s)) {
+    ASSERT_TRUE(false) << "service not available after waiting";
+  }
+
+  auto result = client->async_send_request(request);
+
+  auto ret = rclcpp::spin_until_future_complete(node, result,
+                                                5s);  // Wait for the result.
+  ASSERT_EQ(ret, rclcpp::FutureReturnCode::SUCCESS);
+
+  EXPECT_EQ("Testing Edited by service", result.get()->op);
+}
+}  // namespace minimal_integration_test
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  int result = RUN_ALL_TESTS();
+  rclcpp::shutdown();
+  std::cout << "DONE SHUTTING DOWN ROS" << std::endl;
+  return result;
+}
